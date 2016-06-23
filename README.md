8d50aa64f939df9699a78cae52fe1385beaca272
Christopher Hill <thorsdaddy77@gmail.com>
2016-06-23 13:09:24 -0500

    final commit on jasmine exercise

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # writing-math-tests
+Write unit tests to check for the existence of the following functions:
+add()
+subtract()
+multiply()
+divide()
+square()
+squareRoot()
+Write unit tests that will verify the expected output of each of those functions.
+User Interface
+
+Create a user interface for a calculator. One text input and 6 buttons.
+The user will enter a number in the text input.
+The user will then press one of the buttons for an operation.
+The input field should be cleared so that a new number can be typed in.
+The user will then type in a new number and press enter.
+The resulting value of the operation should then be in the text input.
---
+++ spec/MathSpec.js
@@ -0,0 +1,56 @@
+// describe("The specification for basic math", function() {
+
+//     it("should have an add function", function() {
+//         expect(add).toBeDefined();
+//     });
+
+//     it("should add two integers", function() {
+//         expect(add(2, 5)).toBe(7);
+//     });
+
+//     it("should have a subtract function", function() {
+//         expect(subtract).toBeDefined();
+//     });
+
+//     it("should subtract two integers", function() {
+//         expect(subtract(7, 5)).toBe(2);
+//     });
+
+//     it("should have an multiply function", function() {
+//         expect(multiply).toBeDefined();
+//     });
+
+//     it("should multiply two integers", function() {
+//         expect(multiply(2, 3)).toBe(6);
+//     })
+
+//     it("should have an divide function", function() {
+//         expect(divide).toBeDefined();
+//     });
+
+//     it("should divide two integers", function() {
+//         expect(divide(6, 2)).toBe(3);
+//     })
+
+//     it("should have an square function", function() {
+//         expect(square).toBeDefined();
+//     });
+
+//     it("should square an integer", function() {
+//         expect(Math.pow(7, 2)).toBe(49);
+//     })
+
+//     it("should have an squareRoot function", function() {
+//         expect(squareRoot).toBeDefined();
+//     });
+
+//     it("should get the squareRoot of a number", function() {
+//         expect(Math.sqrt(4)).toBe(2);
+//     })
+
+
+
+
+
+
+// });
